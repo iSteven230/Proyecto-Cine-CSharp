403bb452c5ab8cc7b1b8d7033c8cee5a010bb6b6
Steven <75346434+itsSteven23@users.noreply.github.com>
2021-11-22 14:40:12 -0500

    Update ProgramaCine.cs

--- Cine/ProgramaCine.cs
+++ Cine/ProgramaCine.cs
@@ -5,12 +5,11 @@ namespace Cine
     class ProgramaCine
     {
 
-        
+        //programa para ejecutar el programa
         static void Main(string[] args)
         {
 
-            Menu.menu();
-            SalaJhonWick.imprimirsala();
+            Menu.menu();            //llama a la clase Menu para abrir el menu
         }
     }
 }
