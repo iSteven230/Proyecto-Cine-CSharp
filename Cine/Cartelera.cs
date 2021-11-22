251ca3e02007f8091e56f10b6ffd63d692ec4e81
Steven <75346434+itsSteven23@users.noreply.github.com>
2021-11-22 14:22:22 -0500

    Update Cartelera.cs

--- Cine/Cartelera.cs
+++ Cine/Cartelera.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace Cine
 {
+    //en esta clase encuentra varios metodos, que sirve para ver la cartelera, etc
     class Cartelera
     {
-
+        //sirve para ingresar a la cartelera del cine
         public static void cartelera()
         {
             int opcion;
@@ -23,50 +24,51 @@ namespace Cine
             switch (opcion)
             {
                 case 1:
-                    Console.Clear();
-                    SalaJhonWick.Menusalajhonwick();
+                    Console.Clear();                        //sirve para quitar las lineas de la consola
+                    SalaJhonWick.Menusalajhonwick();        //si digita 1 se va a la sala de Jhon Wick
                     break;
 
                 case 2:
-                    Console.Clear();
-                    SalaAladdin.Menusalaaladdin();
+                    Console.Clear();                        //sirve para quitar las lineas de la consola
+                    SalaAladdin.Menusalaaladdin();          //si digita 2 se va a la sala de Aladdin
                     break;
 
                 case 3:
-                    Console.Clear();
-                    SalaAvengers.Menusalaavengers();
+                    Console.Clear();                        //sirve para quitar las lineas de la consola
+                    SalaAvengers.Menusalaavengers();        //si digita 3 se va a la sala de Avengers
                     break;
 
                 case 4:
-                    Console.Clear();
-                    SalaPikachu.Menusalapikachu();
+                    Console.Clear();                        //sirve para quitar las lineas de la consola
+                    SalaPikachu.Menusalapikachu();          //si digita 4  se va a la sala de Pikachu
                     break;
 
                 case 5:
-                    Menu.menu();
+                    Menu.menu();                            //si digita 5, se va a la clase Menu, quiere decir que va a retroceder en el programa
                     break;
 
                 default:
-
+                                                            //Si dijita otro numero, repite
                     break;
             }
         }
-        public static void comprarboletojhonwick()
+        //metodo que sirve para comprar el boleto de Jhon Wick 3
+        public static void comprarboletojhonwick()  
         {
             bool flag;
             int comprarfila;
             int comprarcolumna;
             do
             {
-
                 Console.WriteLine("digite el numero de fila");
                 comprarfila = int.Parse(Console.ReadLine());
                 Console.WriteLine("digite el numero de columna");
                 comprarcolumna = int.Parse(Console.ReadLine());
-                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);
-            } while (!flag);
-                SalaJhonWick.comprarboletojhonwick(comprarfila,comprarcolumna);
+                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);     //guarda las fila y columnas para ver comprobar en la siguiente linea si se encuentra disponible
+            } while (!flag);                                                            //comprobar
+                SalaJhonWick.comprarboletojhonwick(comprarfila,comprarcolumna);         //devuelve los datos a la Sala Jhon Wick al metodo comprarboleto
         }
+        //metodo que sirve para comprar el boleto de Aladdin
         public static void comprarboletoaladdin()
         {
             bool flag;
@@ -78,10 +80,11 @@ namespace Cine
                 comprarfila = int.Parse(Console.ReadLine());
                 Console.WriteLine("digite el numero de columna");
                 comprarcolumna = int.Parse(Console.ReadLine());
-                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);
-            } while (!flag);
-            SalaJhonWick.comprarboletojhonwick(comprarfila, comprarcolumna);
+                flag = SalaAladdin.verificarasientos(comprarfila, comprarcolumna);      //guarda las fila y columnas para ver comprobar en la siguiente linea si se encuentra disponible
+            } while (!flag);                                                            //comprobar
+            SalaAladdin.comprarboletoaladdin(comprarfila, comprarcolumna);              //devuelve los datos a la Sala Aladdin al metodo comprarboleto
         }
+        //metodo que sirve para comprar el boleto de Avengers
         public static void comprarboletoavengers()
         {
             bool flag;
@@ -93,10 +96,11 @@ namespace Cine
                 comprarfila = int.Parse(Console.ReadLine());
                 Console.WriteLine("digite el numero de columna");
                 comprarcolumna = int.Parse(Console.ReadLine());
-                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);
-            } while (!flag);
-            SalaJhonWick.comprarboletojhonwick(comprarfila, comprarcolumna);
+                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);     //guarda las fila y columnas para ver comprobar en la siguiente linea si se encuentra disponible
+            } while (!flag);                                                            //comprobar
+            SalaAvengers.comprarboletoavengers(comprarfila, comprarcolumna);            //devuelve los datos a la Sala Avengers al metodo comprarboleto
         }
+        //metodo que sirve para comprar el boleto de Pikachu
         public static void comprarboletopikachu()
         {
             bool flag;
@@ -108,9 +112,9 @@ namespace Cine
                 comprarfila = int.Parse(Console.ReadLine());
                 Console.WriteLine("digite el numero de columna");
                 comprarcolumna = int.Parse(Console.ReadLine());
-                flag = SalaJhonWick.verificarasientos(comprarfila, comprarcolumna);
-            } while (!flag);
-            SalaJhonWick.comprarboletojhonwick(comprarfila, comprarcolumna);
+                flag = SalaPikachu.verificarasientos(comprarfila, comprarcolumna);      //guarda las fila y columnas para ver comprobar en la siguiente linea si se encuentra disponible
+            } while (!flag);                                                            //comprobar
+            SalaPikachu.comprarboletopikachu(comprarfila, comprarcolumna);              //devuelve los datos a la Sala Pikachu al metodo comprarboleto
         }
     }
 }
